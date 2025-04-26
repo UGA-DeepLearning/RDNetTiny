280ff9660882fc9a24719202ef9b2091e40b5e0b
alex darwiche <59787022+alexander-darwiche@users.noreply.github.com>
2025-04-26 01:35:06 -0400

    Update inference.py

--- scripts/inference.py
+++ scripts/inference.py
@@ -8,7 +8,7 @@ from timm import create_model
 model = create_model("rdnet_tiny", pretrained=False, num_classes=10)
 
 # Step 2: Load checkpoint
-checkpoint = torch.load('/content/RDNetTiny/scripts/rdnet_tiny_transfer_learn__valLoss0.1992_valAcc93.86.pth', weights_only=False)
+checkpoint = torch.load('/content/RDNetTiny/scripts/rdnet_tiny_transfer_learn__valLoss0.1992_valAcc93.86.pth', weights_only=True)
 
 # Step 3: Extract only the model weights
 state_dict = checkpoint['model_state_dict']
